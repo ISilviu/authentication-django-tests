e204a92c6026f0e3be951bceacbc30f9812ce899
Ifrim Silviu <ifrim.silviu2@gmail.com>
2023-02-11 10:45:04 +0200

    Add count test.

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -1,10 +1,13 @@
-# tests.py, at the same level as serializers.py
 from rest_framework.test import APITestCase
 from rest_framework import status
 
 class UserApiTests(APITestCase):
     base_url = '/users/'
-
+    
     def test_get_users(self):
         status_code = self.client.get(self.base_url).status_code
         self.assertEqual(status_code, status.HTTP_200_OK)
+
+    def test_users_count(self):
+        users_list = self.client.get(self.base_url).data
+        self.assertEqual(len(users_list), 0)
