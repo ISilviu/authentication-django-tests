fe010f747c37039fe0f3932f6b120f913e0dc2c8
Ifrim Silviu <ifrim.silviu2@gmail.com>
2023-02-11 10:28:17 +0200

    Add first test.

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+# tests.py, at the same level as serializers.py
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class UserApiTests(APITestCase):
+    base_url = '/users/'
+
+    def test_get_users(self):
+        status_code = self.client.get(self.base_url).status_code
+        self.assertEqual(status_code, status.HTTP_200_OK)
