d9b1e1b41c224fb5aaacd9f7ea8ec8e442e0f99a
Ifrim Silviu <ifrim.silviu2@gmail.com>
2023-02-11 10:28:28 +0200

    Add Auth mixin.

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -2,19 +2,22 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from django.conf import settings
 
-class UserViewSet(viewsets.ModelViewSet):
+class AuthenticationMixin:
+    permission_classes = [
+        permissions.IsAuthenticated if settings.ENABLE_AUTHENTICATION else permissions.AllowAny]
+
+class UserViewSet(AuthenticationMixin, viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(AuthenticationMixin, viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
